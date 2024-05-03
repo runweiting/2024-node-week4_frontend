da5a3a5c5815180c9d46251316edfd445ab928c1
runweiting <whatisfuntime@gmail.com>
2024-05-03 15:22:00 +0800

    feat: 🎸 index.html 修改 bg-light

--- src/stores/front/userPostsStore.js
+++ src/stores/front/userPostsStore.js
@@ -34,7 +34,7 @@ const userPostsStore = defineStore("userPostsStore", {
       const loader = $loading.show();
       try {
         axios.post(VITE_APP_URL, {
-          user: "662a6bacbf706db5129b49f8",
+          user: "662a6bacbf706db5129b49f9",
           content: tempContent,
           image: tempImage,
           likes: "",
